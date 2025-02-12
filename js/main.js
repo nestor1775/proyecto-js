68d00549c64d51ecc60d91817e52cfbec0e3f2bd
nestor <nestorgomez123450@gmail.com>
2025-02-12 19:36:19 +0100

    nuevas cosas

---
+++ js/main.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    cargarTarjetas();
+});
+
+async function cargarTarjetas() {
+    try {
+        const response = await fetch('https://digi-api.com/api/v1/digimon?pageSize=15');
+        const data = await response.json();
+    
+        const contenedor = document.getElementById('tarjetasDigi');
+    
+    if (!contenedor) {
+        throw new Error("No se encontró el contenedor");
+    }
+
+    contenedor.innerHTML = "";
+
+    data.content.forEach(digimon => {
+        const card = document.createElement('div');
+        card.classList.add('Tarjeta');
+        card.setAttribute('data-tilt', '');
+        
+        card.innerHTML = `
+        <img src="${digimon.image}" alt="${digimon.name}">
+        <p>${digimon.name}</p>
+        <button class="aboutDigi" data-id="${digimon.id}" >Aprende sobre mí</button>
+        
+        `;
+        
+        contenedor.appendChild(card);
+    });
+
+
+      // Necesito agregar esto para que la librerira de las tarjetas funcione
+    VanillaTilt.init(document.querySelectorAll(".Tarjeta"));
+    } catch (error) {
+    console.error("Error cargando tarjetas:", error);
+    }
+}
+
+
+
+
+const button = card.querySelector('.aboutDigi');
+button.addEventListener('click', () => {
+    window.location.href = `../moreinfo?id=${id}`;;
+});
